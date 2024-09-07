5b4ed3a94bd51a27aa2d7915693611edd753ed8b
Johnathan Spiva <spivajohnathan64@gmail.com>
2024-09-07 13:07:17 -0700

    Updated to the latest version of alsa-utils

---
+++ pullpushalsaplugins.sh
@@ -0,0 +1,1 @@
+git remote remove upstream && git remote add upstream https://github.com/alsa-project/alsa-utils.git && git pull upstream master && git remote remove upstream && git remote add upstream https://gitlab.com/acreetionos-developers1/alsa-utils.git && git add . && git commit -m "Updated to the latest version of alsa-utils" &&  git push
